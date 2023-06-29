106c92ab7f4882f99a317196258c022717379120
rihabalyasiri <rihab.al-yasiri@web.de>
2023-06-29 21:47:40 +0200

    gender logic finished

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:bmi_calculator/card.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:bmi_calculator/rounded_button.dart';
+import 'package:bmi_calculator/genders.dart';
 
 const Color containerBgColor = Color(0xFF272A4E);
 const textStyle =
@@ -10,57 +11,91 @@ const textStyle =
 const numberStyle =
     TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 70);
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Color activeColor = Colors.white30;
+  Color inactiveColor = Color(0xFF272A4E);
+  Color maleColor = Color(0xFF272A4E);
+  Color femaleColor = Color(0xFF272A4E);
+
+  void activeGender(Gender selectedGender) {
+    if(selectedGender == Gender.MALE) {
+        maleColor = activeColor;
+        femaleColor = inactiveColor;
+    }else {
+      maleColor = inactiveColor;
+      femaleColor = activeColor;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Column(
         children: [
-          const Expanded(
+          Expanded(
             child: Row(
               children: [
                 Expanded(
-                    child: ReusableCard(
-                  colour: containerBgColor,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      FaIcon(
-                        FontAwesomeIcons.mars,
-                        color: Colors.white,
-                        size: 80,
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Text(
-                        "MALE",
-                        style: textStyle,
-                      )
-                    ],
+                    child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      activeGender(Gender.MALE);
+                    });
+                  },
+                  child: ReusableCard(
+                    colour: maleColor,
+                    child: const Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        FaIcon(
+                          FontAwesomeIcons.mars,
+                          color: Colors.white,
+                          size: 80,
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          "MALE",
+                          style: textStyle,
+                        )
+                      ],
+                    ),
                   ),
                 )),
                 Expanded(
-                    child: ReusableCard(
-                  colour: containerBgColor,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      FaIcon(
-                        FontAwesomeIcons.venus,
-                        color: Colors.white,
-                        size: 80,
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Text(
-                        "FEMALE",
-                        style: textStyle,
-                      )
-                    ],
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          activeGender(Gender.FEMALE);
+                        });
+                      },
+                  child:  ReusableCard(
+                    colour: femaleColor,
+                    child: const Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        FaIcon(
+                          FontAwesomeIcons.venus,
+                          color: Colors.white,
+                          size: 80,
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          "FEMALE",
+                          style: textStyle,
+                        )
+                      ],
+                    ),
                   ),
                 ))
               ],
@@ -85,8 +120,7 @@ class HomeScreen extends StatelessWidget {
                         "180",
                         style: numberStyle,
                       ),
-                      Text("cm",
-                          style:textStyle)
+                      Text("cm", style: textStyle)
                     ]),
                 Slider(
                   value: 160,
@@ -99,7 +133,7 @@ class HomeScreen extends StatelessWidget {
               ],
             ),
           )),
-           Expanded(
+          Expanded(
             child: Row(
               children: [
                 Expanded(
@@ -109,36 +143,60 @@ class HomeScreen extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       const Text(
-                        "WEIGHT", style: textStyle,
+                        "WEIGHT",
+                        style: textStyle,
                       ),
-                      const Text("60", style: numberStyle,),
+                      const Text(
+                        "60",
+                        style: numberStyle,
+                      ),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          RoundedButton(icon: FontAwesomeIcons.minus, onPressed:() {} ,),
-                          const SizedBox(width: 15,),
-                          RoundedButton(icon: FontAwesomeIcons.plus, onPressed:() {} ,)
+                          RoundedButton(
+                            icon: FontAwesomeIcons.minus,
+                            onPressed: () {},
+                          ),
+                          const SizedBox(
+                            width: 15,
+                          ),
+                          RoundedButton(
+                            icon: FontAwesomeIcons.plus,
+                            onPressed: () {},
+                          )
                         ],
                       )
                     ],
                   ),
                 )),
-                 Expanded(
+                Expanded(
                     child: ReusableCard(
                   colour: containerBgColor,
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       const Text(
-                        "AGE", style: textStyle,
+                        "AGE",
+                        style: textStyle,
                       ),
-                      const Text("20", style: numberStyle,),
+                      const Text(
+                        "20",
+                        style: numberStyle,
+                      ),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          RoundedButton(icon: FontAwesomeIcons.minus, onPressed:() {} ,),
-                          const SizedBox(width: 15,),
-                          RoundedButton(icon: FontAwesomeIcons.plus, onPressed:() {} ,)
+                          RoundedButton(
+                            icon: FontAwesomeIcons.minus,
+                            onPressed: () {},
+                          ),
+                          const SizedBox(
+                            width: 15,
+                          ),
+                          RoundedButton(
+                            icon: FontAwesomeIcons.plus,
+                            onPressed: () {},
+                          )
                         ],
                       )
                     ],
